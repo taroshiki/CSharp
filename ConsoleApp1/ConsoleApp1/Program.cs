70b7a42036b2332c52ad1565c9bef0250fe7323a
taros <taros@DESKTOP-OJA02NG>
2019-04-13 10:09:02 +0900

    add characters class

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,8 +10,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Greeting(new System.Random().Next(3));
-            ///　0 = ベイマックス2
+            Characters characters = new Characters();
+            characters.Greeting(new System.Random().Next(3));
+            ///　0 = ベイマックス
             ///　1 = オラフ
             ///　2 = ジーニー
             ///  
@@ -20,35 +21,7 @@ namespace ConsoleApp1
 
         }
 
-        static void Greeting(int CharaNum)
-        {
-            string name;
-            switch (CharaNum)
-            {
-                case 0:
-                    Console.WriteLine("こんにちは、私はベイマックス。あなたの健康を守ります。");
-                    Console.WriteLine("あなたのお名前は？");
-                    name = Console.ReadLine();
-                    Console.WriteLine($"{name}ですね、登録しました。よろしくお願いします。");
-                    break;
-                case 1:
-                    Console.WriteLine("やぁ！僕はオラフ。ぎゅーって抱きしめて！");
-                    Console.WriteLine("君の名前は？");
-                    name = Console.ReadLine();
-                    Console.WriteLine($"{name}か!よろしくね！そっちのスヴェンもよろしくね。");
-                    break;
-                case 2:
-                    Console.WriteLine("オイ！1万年もじーっとしてたから首がガッチガチだわさ");
-                    Console.WriteLine("あんたがおいらのご主人様か！名前はなんてんだい？");
-                    name = Console.ReadLine();
-                    Console.WriteLine($"{name}ってのか!ご挨拶ご挨拶");
-                    break;
-                default:
-                    break;
-            }
-
-            Console.ReadLine();
-        }
+        
 
     }
 }
